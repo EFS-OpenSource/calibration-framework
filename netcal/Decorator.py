232aa57b4e3320a79e6029c7d5f087a6dab7bbdb
Fabian Küppers <fabian.kueppers@hs-ruhrwest.de>
2021-06-30 14:21:36 +0200

    Update decorator by torch arrays

--- netcal/Decorator.py
+++ netcal/Decorator.py
@@ -1,14 +1,13 @@
-"""
-Copyright (C) 2019-2021 Ruhr West University of Applied Sciences, Bottrop, Germany
-AND Elektronische Fahrwerksysteme GmbH, Gaimersheim Germany
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""
+# Copyright (C) 2019-2021 Ruhr West University of Applied Sciences, Bottrop, Germany
+# AND Elektronische Fahrwerkssysteme, Gaimersheim, Germany
+#
+# This Source Code Form is subject to the terms of the Apache License 2.0
+# If a copy of the APL2 was not distributed with this
+# file, You can obtain one at https://www.apache.org/licenses/LICENSE-2.0.txt.
 
 
 import numpy as np
+import torch
 from functools import wraps
 
 
@@ -17,7 +16,7 @@ def accepts(*types):
     Decorator for function arg check
     """
     def check_accepts(f):
-        assert len(types)+1 == f.__code__.co_argcount
+        assert len(types)+1 == f.__code__.co_argcount, "Unequal amount of defined parameter types and existing parameters."
 
         @wraps(f)
         def new_f(*args, **kwds):
@@ -45,7 +44,7 @@ def dimensions(*dim):
     Decorator for numpy array dimension check
     """
     def check_dim(f):
-        assert len(dim)+1 == f.__code__.co_argcount
+        assert len(dim)+1 == f.__code__.co_argcount, "Unequal amount of defined dimensions and existing parameters."
 
         @wraps(f)
         def new_f(*args, **kwds):
@@ -54,7 +53,7 @@ def dimensions(*dim):
                 if d is None:
                     continue
 
-                assert isinstance(a, np.ndarray), "arg \'%s\' does not match %s" % (f.__code__.co_varnames[i], np.ndarray)
+                assert isinstance(a, (np.ndarray, torch.Tensor, )), "arg \'%s\' does not match %s or %s" % (f.__code__.co_varnames[i], np.ndarray, torch.Tensor)
 
                 if type(d) == tuple:
                     assert len(a.shape) in d, "dimension of arg \'%s\' must match %s but is %d" % (f.__code__.co_varnames[i], str(d), len(a.shape))
@@ -72,7 +71,7 @@ def global_accepts(*types):
     Decorator for global function's arg check
     """
     def check_accepts(f):
-        assert len(types) == f.__code__.co_argcount
+        assert len(types) == f.__code__.co_argcount, "Unequal amount of defined parameter types and existing parameters."
 
         @wraps(f)
         def new_f(*args, **kwds):
@@ -100,7 +99,7 @@ def global_dimensions(*dim):
     Decorator for global function's numpy array dimension check
     """
     def check_dim(f):
-        assert len(dim) == f.__code__.co_argcount
+        assert len(dim) == f.__code__.co_argcount, "Unequal amount of defined dimensions and existing parameters."
 
         @wraps(f)
         def new_f(*args, **kwds):
@@ -109,7 +108,7 @@ def global_dimensions(*dim):
                 if d is None:
                     continue
 
-                assert isinstance(a, np.ndarray), "arg \'%s\' does not match %s" % (f.__code__.co_varnames[i], np.ndarray)
+                assert isinstance(a, (np.ndarray, torch.Tensor)), "arg \'%s\' does not match %s or %s" % (f.__code__.co_varnames[i], np.ndarray, torch.Tensor)
 
                 if type(d) == tuple:
                     assert len(a.shape) in d, "dimension of arg \'%s\' must match %s but is %d" % (f.__code__.co_varnames[i], str(d), len(a.shape))
